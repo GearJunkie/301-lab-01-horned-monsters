2ab1ed07447ec0a98794e245817b330d5c204ebd
jesse dills <jessedills@yahoo.com>
2021-07-14 19:25:34 -0400

    stuff n things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Header from "./header.js";
-import Main from "./main.js";
-import Footer from "./footer.js";
-
+import React from 'react';
+import Header from './header.js';
+import Main from './main.js';
+import Footer from './footer.js';
+import './App.css';
 
 class App extends React.Component {
   render() {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import React from "react";
-import data from "./data.json";
-import HornedBeasts from "./HornedBeasts.js";
-
+import React from 'react';
+import data from './data.json';
+import HornedBeasts from './HornedBeasts.js';
+import {Container} from 'react-bootstrap';
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -11,16 +11,23 @@ class Main extends React.Component {
   }
 
    beastArr = (data) => {
-     let newArr = data.map( (value) => {
-       return <HornedBeasts title={value.title} image_url={value.image_url} description={value.description} horns={value.horns}/>;
-     });
+     let newArr = data.map( (value) => 
+       <HornedBeasts 
+         title={value.title} 
+         image_url={value.image_url} 
+         description={value.description} 
+         horns={value.horns}
+         key={value.image_url}/>
+     );
      return newArr;
    };
 
    render() {
      return (
        <div className = "main">
-         {this.beastArr(data)}
+         <Container>
+           {this.beastArr(data)}
+         </Container>
        </div>
      );
    }

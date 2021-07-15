508273f8978e2470d170966b01fc53e2612598cf
jesse dills <jessedills@yahoo.com>
2021-07-15 19:50:23 -0400

    complete lab 03 finally

--- src/main.js
+++ src/main.js
@@ -1,42 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardColumns from 'react-bootstrap/CardColumns';
 import HornedBeasts from './HornedBeasts.js';
-// import {Card, Container} from 'react-bootstrap';
+
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
+ 
   //------My render beasts cards--------//
-//   render () {
-//     return(
-//       <Card style={{ width: '18rem' }}>
-//   <Card.Img variant="top" src="image_url" />
-//   <Card.Body>
-//     <Card.Title>{this.props.title}</Card.Title>
-//     </Card.Text>
-//     <Button variant="primary">Go somewhere</Button>
-//   </Card.Body>
-// </Card>
-//     )
-//   }
-
-  //-------My render beasts array--------//
+ 
   render() {
     return (
-      <div className = "main">
-        <Container>
-          {this.props.data.map( (value) =>
+      <div id="beastCards">
+        <CardColumns>
+          {this.props.beasts.map( (beast, index) =>
             <HornedBeasts
-              title={value.title}
-              image_url={value.image_url}
-              description={value.description}
-              horns={value.horns}
-              key={value.image_url}/>
+              title={beast.title}
+              image={beast.image_url}
+              description={beast.description}
+              horns={beast.horns}
+              key={index}
+              displayAsModal={this.props.displayAsModal}
+            />
           )};
-        </Container>
+        </CardColumns>
       </div>
     );
   }

831cf5dad1e9a2b4f77d627ebf76d146086544f9
jesse dills <jessedills@yahoo.com>
2021-07-14 22:00:06 -0400

    shifted state to App, adjusted beastArr in Main

--- src/main.js
+++ src/main.js
@@ -1,36 +1,45 @@
 import React from 'react';
-import data from './data.json';
 import HornedBeasts from './HornedBeasts.js';
-import {Container} from 'react-bootstrap';
+// import {Card, Container} from 'react-bootstrap';
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data
+
     };
   }
 
-   beastArr = (data) => {
-     let newArr = data.map( (value) =>
-       <HornedBeasts
-         title={value.title}
-         image_url={value.image_url}
-         description={value.description}
-         horns={value.horns}
-         key={value.image_url}/>
-     );
-     return newArr;
-   };
+  //------My render beasts cards--------//
+//   render () {
+//     return(
+//       <Card style={{ width: '18rem' }}>
+//   <Card.Img variant="top" src="image_url" />
+//   <Card.Body>
+//     <Card.Title>{this.props.title}</Card.Title>
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+//   </Card.Body>
+// </Card>
+//     )
+//   }
 
-   render() {
-     return (
-       <div className = "main">
-         <Container>
-           {this.beastArr(data)}
-         </Container>
-       </div>
-     );
-   }
+  //-------My render beasts array--------//
+  render() {
+    return (
+      <div className = "main">
+        <Container>
+          {this.props.data.map( (value) =>
+            <HornedBeasts
+              title={value.title}
+              image_url={value.image_url}
+              description={value.description}
+              horns={value.horns}
+              key={value.image_url}/>
+          )};
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Main;
